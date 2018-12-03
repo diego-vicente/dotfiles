bfc9d7845ad4371d6bd2d12112d24f27afc196dd
Diego Vicente <mail@diego.codes>
2018-12-03 12:34:55 +0100

    Move scripts folder out of i3 and fix references

--- scripts/set-work-monitor.sh
+++ scripts/set-work-monitor.sh
@@ -3,8 +3,9 @@
 # xrandr | grep 'HDMI1 connected' &&
 
 xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x392 --rotate normal \
-       --output HDMI1 --mode 1440x900 --pos 1920x0 --rotate normal --set "Broadcast RGB" "Limited 16:235"
+       --output HDMI1 --mode 1440x900 --pos 1920x0 --rotate normal
 
+# --set "Broadcast RGB" "Limited 16:235"
 
 pactl set-card-profile 0 output:analog-stereo
 
