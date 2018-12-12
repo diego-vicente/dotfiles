a1440db3593f115f907efd7e8132d1c49e9f0855
Diego Vicente <mail@diego.codes>
2018-12-12 10:57:21 +0100

    Add xfce4 terminal configuration

--- setup.sh
+++ setup.sh
@@ -7,12 +7,13 @@ else
     DEST=$XDG_CONF_HOME
 fi
 
-ln -s $HOME/dotfiles/compton/config   $DEST/compton.conf
-ln -s $HOME/dotfiles/dunst/dunstrc    $DEST/dunst/dunstrc
-ln -s $HOME/dotfiles/i3/config        $DEST/i3/config
-ln -s $HOME/dotfiles/polybar/config   $DEST/polybar/config
-ln -s $HOME/dotfiles/X/.Xmodmap       $HOME/.Xmodmap
-ln -s $HOME/dotfiles/X/.Xresources    $HOME/.Xresources
+ln -s $HOME/dotfiles/compton/config            $DEST/compton.conf
+ln -s $HOME/dotfiles/dunst/dunstrc             $DEST/dunst/dunstrc
+ln -s $HOME/dotfiles/i3/config                 $DEST/i3/config
+ln -s $HOME/dotfiles/xfce4-terminal/terminalrc $DEST/xfce4/terminal/terminalrc
+ln -s $HOME/dotfiles/polybar/config            $DEST/polybar/config
+ln -s $HOME/dotfiles/X/.Xmodmap                $HOME/.Xmodmap
+ln -s $HOME/dotfiles/X/.Xresources             $HOME/.Xresources
 
 echo "Symbolic links in $DEST created"
 
