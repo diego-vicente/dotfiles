55225cf7bde35143f7776cf9e025dcaabdd17f90
Diego Vicente <mail@diego.codes>
2019-01-08 15:31:21 +0100

    [WIP] Launch work polybars

--- scripts/start-polybar.sh
+++ scripts/start-polybar.sh
@@ -6,8 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar bar-hdmi &
+# Launch all the bars
+polybar bar-hdmi-home &
+polybar bar-hdmi-work &
 polybar bar-laptop &
 
 echo "Bars launched..."
