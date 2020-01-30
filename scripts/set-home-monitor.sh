fc68bcf713ab267a71b89e352e9aba1d5019cc80
Diego Vicente <mail@diego.codes>
2020-01-30 10:54:45 +0100

    Reorganize the dotfiles for multi-folder
    Instead of using a single configuration mangled with several environment
    variables, this setup will aim to have multiple folders that define each
    of the devices by their hostnames. This allows symbolic linking easily
    and results in easier to debug configurations.
    
    This is also due to the fact that I am moving from using Antergos
    everywhere to a setup composed of Debian at work and Void Linux at home,
    two distributions that are very different from each other.

--- scripts/set-home-monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# xrandr | grep 'HDMI1 connected' &&
-xrandr --output $LAPTOP --mode 1920x1080 --pos 2560x576 --rotate normal \
-       --output $HDMI --primary --mode 2560x1440 --pos 0x0 --rotate normal --set "Broadcast RGB" "Full"
-
-pactl set-card-profile 0 output:hdmi-stereo
-
-i3-msg restart
--- scripts/set-single-monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# xrandr | grep 'HDMI1 connected' &&
-    xrandr --output $LAPTOP --primary --mode 1920x1080 --rotate normal \
-           --output $HDMI --off \
-
-pactl set-card-profile 0 output:analog-stereo
-
-i3-msg restart
