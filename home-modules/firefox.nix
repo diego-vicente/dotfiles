06dec90f5faf5b944eb0cae23e9618b2109e0d02
Diego Vicente <mail@diego.codes>
2020-09-20 13:30:01 +0200

    Fix the download folder for Firefox

--- home-modules/firefox.nix
+++ home-modules/firefox.nix
@@ -10,6 +10,8 @@
         "browser.bookmarks.showMobileBookmarks" = true;
         "browser.fullscreen.autohide" = false;
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "browser.download.dir" = "~/docs/downloads";
+        "browswer.download.folderList" = 2;
         "browser.aboutConfig.showWarning" = false;
         "sidebar.position_start" = false;
         # Recommended by Tree Style Tab settings
