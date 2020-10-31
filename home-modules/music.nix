239dab09478ff4c134f7079d9aeb2cb73a74a8ee
Diego Vicente <mail@diego.codes>
2020-10-31 14:07:13 +0100

    Pin music.nix to NixOS 20.09

--- home-modules/music.nix
+++ home-modules/music.nix
@@ -1,8 +1,20 @@
 { config, lib, pkgs, unstable, ... }:
 
 {
-  home.packages = [
-    # FIXME: unstable.vcv-rack
-    unstable.orca-c
+  home.packages = with pkgs; [
+    vcv-rack
+    orca-c
   ];
+
+  # Add a desktop entry for GPU enabled VCV
+  home.file.".local/share/applications/Rack.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Encoding=UTF-8
+    Name=VCV Rack
+    Comment=An open-source virtual module synthesizer.
+    Exec=nvidia-offload ${pkgs.vcv-rack}/bin/Rack
+    Icon=application.png
+    Terminal=false
+  '';
 }
