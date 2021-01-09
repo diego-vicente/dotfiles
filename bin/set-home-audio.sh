f9dc45fd641aa78f57631f9d472d46b60e435b52
Diego Vicente <mail@diego.codes>
2021-01-09 12:05:17 +0000

    Remove unused scripts

--- bin/set-home-audio.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# TODO: store this values in environment variables?
-pactl set-card-profile 0 output:hdmi-stereo+input:analog-stereo
-pactl set-sink-port alsa_output.pci-0000_00_1f.3.hdmi-stereo hdmi-output
-pactl set-source-port alsa_input.pci-0000_00_1f.3.analog-stereo analog-input-internal-mic
