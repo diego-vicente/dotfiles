92bc0590d07ed35522d86717b362f7153489f251
Diego Vicente <mail@diego.codes>
2021-01-10 12:29:22 +0100

    Fix some soyuz specific attributes
    Apart from that, this commit also includes some help to understand the
    host-specific attributes and how to find the appropriate values

--- bootstrap/soyuz.nix
+++ bootstrap/soyuz.nix
@@ -9,36 +9,64 @@ let
   additionalArgs = {
     hostname = "soyuz";
     hostSpecific = {
-      # The video output names and its xrandr arguments
       info = {
+        # --- Basic user information ---
+        # This set contains some general information to be associated with each
+        # machine. For example, the name and email here will the the one used by
+        # default in git commits across the system.
         userName = "Diego Vicente";
         userEmail = "diego.vicente@decidesoluciones.es";
       };
       video = {
+        # --- Video outputs ---
+        # The output names differ from one system to another and can be checked
+        # using:
+        #   $ xrandr
+        # If the setup is the same, the same `xrandrArgs` should be valid from
+        # one system to another.
         laptop = {
           output = "eDP-1";
           xrandrArgs = "--mode 1920x1080 --rotate normal";
         };
         hdmi = {
-          output = "HDMI-1";
+          output = "DP-3";
           xrandrArgs = ''--mode 2560x1440 --rotate normal --set "Broadcast RGB" "Full"'';
         };
+        # --- PCI addresses ---
+        # In order to use the NVidia Offload, the bus id must be given in a
+        # special input. To check for the address, run:
+        #   $ lspci
+        # And check [1] to see the correct way to translate the address.
+        #
+        # [1]: https://nixos.wiki/wiki/Nvidia#offload_mode
         pci = {
           intelBusId = "PCI:0:2:0";
           nvidiaBusId = "PCI:1:0:0";
         };
       };
-      # Network interfaces names
       network = {
+        # --- Network interfaces ---
+        # Which network interfaces are available. It is quite easy to see all of
+        # them using:
+        #   $ ifconfig -a
         wireless = "wlp59s0";
         ethernet = "enp58s0u1u2";
       };
-      # Temperature sensors to be used with lm-sensors
       temperature = {
-        package = "/sys/devices/virtual/hwmon/hwmon3/temp1_input";
+        # --- Temperature sensors ---
+        # Find the package temperature to be displayed on the bar using
+        # `sensors` to see the appropriate measurement and then run the
+        # following script to find the associated descriptor:
+        #   $ for i in /sys/class/hwmon/hwmon*/temp*_input; do echo "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*})) $(readlink -f $i)"; done
+        package = "/sys/devices/platform/coretemp.0/hwmon/hwmon5/temp1_input";
       };
-      # Audio profiles, sinks and ports to be used with pactl/pacmd
       audio = rec {
+        # --- Audio profiles ---
+        # To check the name of the available, I recommend using `pavucontrol` to
+        # check the working outputs for each system and then trying to locate
+        # the proper profile and port name using `grep` and some elbow grease on
+        # its output:
+        #   $ pacmd list
         laptop = {
           output = {
             profile = "analog-stereo";
