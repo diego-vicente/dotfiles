30748a8f958234047ca215b1320c4b125f1bd35f
Diego Vicente <mail@diego.codes>
2021-07-24 18:09:50 +0200

    Include Roboto fonts as a dependency

--- nixos-modules/fonts.nix
+++ nixos-modules/fonts.nix
@@ -5,9 +5,18 @@
     fontDir.enable = true;
     fonts = with pkgs; [
       iosevka
+      roboto
+      roboto-mono
       font-awesome
+      emacs-all-the-icons-fonts
       noto-fonts-emoji
     ];
-    fontconfig.defaultFonts.emoji = [ "Noto Color Emoji" ];
+
+    fontconfig.defaultFonts = {
+      serif = [ "Roboto Slab" ];
+      sansSerif = [ "Roboto" ];
+      monospace = [ "Roboto Mono" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
   };
 }
