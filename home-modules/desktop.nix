44aa934122daf1b2377887f64d0e6096158eead6
Diego Vicente <mail@diego.codes>
2022-02-10 11:38:51 +0100

    Include rolling release of VSCode

--- home-modules/desktop.nix
+++ home-modules/desktop.nix
@@ -1,12 +1,14 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, unstable, ... }:
 
 {
   # Install other desktop apps without in depth-configuration.
   home.packages = with pkgs; [
     chromium
     calibre
+    unstable.rmview
     xfce.thunar
     xfce.thunar-volman
     xfce.thunar-archive-plugin
+    unstable.vscode
   ];
 }
