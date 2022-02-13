74e22ef5ba2a16ef7a7201b0d2d84f2c17be2ce6
Diego Vicente <mail@diego.codes>
2022-02-13 13:30:41 +0100

    Fix dvicente user to appear listed in the greeter

--- nixos-modules/users.nix
+++ nixos-modules/users.nix
@@ -5,7 +5,13 @@
   # first step out of the box should be to run `sudo passwd $USER`.
   users.mutableUsers = true;
 
+  # Setting this will make both bash and zsh valid shells, so that the users
+  # that choose zsh are not treated as SystemAccounts. There is a chance that
+  # you may need to edit /var/lib/AccountsService/users/<name> to fix it.
+  environment.shells = with pkgs; [ bashInteractive zsh ];
+  
   users.users.dvicente = {
+    description = "Diego Vicente";
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "input" "video" "audio" ];
     shell = pkgs.zsh;
