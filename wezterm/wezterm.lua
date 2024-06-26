98437cdd9f2ed70adf9ebc27f3d380aa232e6882
Diego Vicente <mail@diego.codes>
2024-06-26 20:45:42 +0200

    Add smart-splits configuration

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -240,5 +240,16 @@ config.keys = {
   }
 }
 
+local smart_splits = wezterm.plugin.require(
+  'https://github.com/mrjones2014/smart-splits.nvim'
+)
+
+smart_splits.apply_to_config(config, {
+  direction_keys = { 'h', 'j', 'k', 'l' },
+  modifiers = {
+    move = 'CTRL',
+    resize = 'META',
+  },
+})
 
 return config
